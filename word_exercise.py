18e79fa232a9e70a8c28d5aa4930073209e727bf
gnsxhj <gnsxhj@gmail.com>
2024-02-08 16:16:22 +0800

    exercise of leetcode updated

--- word_exercise.py
+++ word_exercise.py
@@ -34,73 +34,73 @@
 #    doc.save(new_path+"{}".format(file.split("/")[-1]))
 #    print("{}替换完成".format(file))
 # ***********************************************************************************************************
-# import os
-# import docx
-# from docx import Document
-#
-# # 放了一些docx 文件
-# old_file_path = "c:/Users/XUHAIJUN/Documents/SVW_Consulter_Strategie_2023年/人员简历/test/"
-# # 生成新文件后的存放地址
-# new_file_path = "c:/Users/XUHAIJUN/Documents/SVW_Consulter_Strategie_2023年/人员简历/test_new/"
-#
-# replace_dict = {
-#     "信息科技": "软件科技",
-# }
-#
-# def check_and_change(document, replace_dict):
-#     """
-#     遍历word中的所有 paragraphs，在每一段中发现含有key 的内容，就替换为 value 。
-#     （key 和 value 都是replace_dict中的键值对。）
-#     """
-#     for para in document.paragraphs:
-#         for i in range(len(para.runs)):
-#             for key, value in replace_dict.items():
-#                 if key in para.runs[i].text:
-#                     print(key+"-->"+value)
-#                     para.runs[i].text = para.runs[i].text.replace(key, value)
-#     return document
-#
-# def main():
-#     for name in os.listdir(old_file_path):
-#         print(name)
-#         old_file = old_file_path + name
-#         new_file = new_file_path + name
-#         if old_file.split(".")[1] == 'docx':
-#             document = Document(old_file)
-#             document = check_and_change(document, replace_dict)
-#             document.save(new_file)
-#         print("^"*30)
-#
-# if __name__ == '__main__':
-#     main()
-# ***********************************************************************************************************
 import os
-import comtypes.client
-def get_path():
-    # 指定路径
-    path = 'c:/Users/XUHAIJUN/SynologyDrive/English/friends/1/'
-    # 获取所有文件名的列表
-    filename_list = os.listdir(path)
-    # 获取所有word文件名列表
-    wordname_list = [filename for filename in filename_list \
-                     if filename.endswith((".doc", ".docx"))]
-    for wordname in wordname_list:
-        # 分离word文件名称和后缀，转化为pdf名称
-        pdfname = os.path.splitext(wordname)[0] + '.pdf'
-        # 如果当前word文件对应的pdf文件存在，则不转化
-        if pdfname in filename_list:
-            continue
-        # 拼接 路径和文件名
-        wordpath = os.path.join(path, wordname)
-        pdfpath = os.path.join(path, pdfname)
-        #生成器
-        yield wordpath,pdfpath
-def convert_word_to_pdf():
-    word = comtypes.client.CreateObject("Word.Application")
-    word.Visible = 0
-    for wordpath,pdfpath in get_path():
-        newpdf = word.Documents.Open(wordpath)
-        newpdf.SaveAs(pdfpath, FileFormat=17)
-        newpdf.Close()
-if __name__ == "__main__":
-    convert_word_to_pdf()
+import docx
+from docx import Document
+
+# 放了一些docx 文件
+old_file_path = "c:/Users/XUHAIJUN/SynologyDrive/DTAS/项目信息/一汽大众/2024年框架/人员简历/"
+# 生成新文件后的存放地址
+new_file_path = "c:/Users/XUHAIJUN/SynologyDrive/DTAS/项目信息/一汽大众/2024年框架/new/"
+
+replace_dict = {
+    "君昱": "棣拓",
+}
+
+def check_and_change(document, replace_dict):
+    """
+    遍历word中的所有 paragraphs，在每一段中发现含有key 的内容，就替换为 value 。
+    （key 和 value 都是replace_dict中的键值对。）
+    """
+    for para in document.paragraphs:
+        for i in range(len(para.runs)):
+            for key, value in replace_dict.items():
+                if key in para.runs[i].text:
+                    print(key+"-->"+value)
+                    para.runs[i].text = para.runs[i].text.replace(key, value)
+    return document
+
+def main():
+    for name in os.listdir(old_file_path):
+        print(name)
+        old_file = old_file_path + name
+        new_file = new_file_path + name
+        if old_file.split(".")[1] == 'docx':
+            document = Document(old_file)
+            document = check_and_change(document, replace_dict)
+            document.save(new_file)
+        print("^"*30)
+
+if __name__ == '__main__':
+    main()
+# ***********************************************************************************************************
+# import os
+# import comtypes.client
+# def get_path():
+#     # 指定路径
+#     path = 'c:/Users/XUHAIJUN/SynologyDrive/English/friends/1/'
+#     # 获取所有文件名的列表
+#     filename_list = os.listdir(path)
+#     # 获取所有word文件名列表
+#     wordname_list = [filename for filename in filename_list \
+#                      if filename.endswith((".doc", ".docx"))]
+#     for wordname in wordname_list:
+#         # 分离word文件名称和后缀，转化为pdf名称
+#         pdfname = os.path.splitext(wordname)[0] + '.pdf'
+#         # 如果当前word文件对应的pdf文件存在，则不转化
+#         if pdfname in filename_list:
+#             continue
+#         # 拼接 路径和文件名
+#         wordpath = os.path.join(path, wordname)
+#         pdfpath = os.path.join(path, pdfname)
+#         #生成器
+#         yield wordpath,pdfpath
+# def convert_word_to_pdf():
+#     word = comtypes.client.CreateObject("Word.Application")
+#     word.Visible = 0
+#     for wordpath,pdfpath in get_path():
+#         newpdf = word.Documents.Open(wordpath)
+#         newpdf.SaveAs(pdfpath, FileFormat=17)
+#         newpdf.Close()
+# if __name__ == "__main__":
+#     convert_word_to_pdf()
